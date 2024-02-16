8f64bd82775fd15fcf8c9f1a13890d818e947116
Amina Djarfi <m.djarfi.amina@gmail.com>
2024-02-16 22:40:07 +0100

    fix

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 import random
 
+import requests
 from flask import Flask
 from flask import render_template
 from flask import request
@@ -26,13 +27,40 @@ def prompt():
 
 
 def build_response(payload):
+    """ Build the response
+
+    Args:
+        payload (dict): The response from the API
+
+        Returns:
+            str: The response to send to the user
+    """
+
     print("message:", payload)
     list_lang = ['fr', 'en', 'es', 'de', 'it', 'pt', 'nl', 'pl', 'ru', 'ja', 'zh', 'ar']
     lang = random.choice(list_lang)
     return f'<b>La langue detectée est :</b> {lang}'
 
 
-def call_api(payload):
+def call_api(text):
+    """ Call the API and return the response
+        POST https://apicoc.com/detect
+        {
+            "text": "Bonjour, comment ça va ?"
+        }
+    """
+    # call the API
+    url = 'https://apicoc.com/detect'
+    json = {'text': text}
+    response = requests.post(url=url, json=json)
+
+    # check if the response is 200
+    if response.status_code != 200:
+        return 'Une erreur est survenue'
+
+    # get the response
+    payload = response.json()
+
     return build_response(payload)
 
 
